64c61c2e7c8d37298665c35547d48fe7b5c2a53f
pessolatohenrique <pessolatohenrique@gmail.com>
2022-09-19 19:51:11 -0300

    testing factory method

--- __tests__/investiment.test.js
+++ __tests__/investiment.test.js
@@ -1,9 +1,16 @@
 const { InvestimentFactory } = require("../factories");
+const { InvalidFactory } = require("../utils/Errors");
+const { Investiment } = require("../models");
 
 describe("Investiment CRUD", () => {
   test("it should create factory when type is valid", () => {
     const factory = new InvestimentFactory();
     const result = factory.create("FIXED_INCOME");
     expect(result).toHaveProperty("validate");
+  });
+
+  test("it should throw error in factory when type is invalid", () => {
+    const factory = new InvestimentFactory();
+    expect(() => factory.create("INVALID_TYPE")).toThrow(InvalidFactory);
   });
 });
